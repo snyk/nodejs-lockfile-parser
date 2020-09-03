41f0c6df8eb1c462ff1cce10a2a3de99f07bf684
Jakub Mikulas <jakub@snyk.io>
2020-09-03 15:36:34 +0200

    fix: update dep-graph to use upstream lodash

--- lib/parsers/package-lock-parser.ts
+++ lib/parsers/package-lock-parser.ts
@@ -2,7 +2,7 @@ import * as _cloneDeep from 'lodash.clonedeep';
 import * as _isEmpty from 'lodash.isempty';
 import * as _set from 'lodash.set';
 import * as _toPairs from 'lodash.topairs';
-import * as graphlib from '@snyk/graphlib';
+import * as graphlib from 'graphlib';
 import * as uuid from 'uuid/v4';
 import { config } from '../config';
 import { eventLoopSpinner } from 'event-loop-spinner';
