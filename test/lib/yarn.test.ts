c1b3f3e87aa08d2d19cb571689f41bd49a0f5837
Mathilde <mathilde.riou@snyk.io>
2021-10-13 14:59:13 +0100

    fix: test for Unix

--- test/lib/yarn.test.ts
+++ test/lib/yarn.test.ts
@@ -153,7 +153,8 @@ for (const scenario of SCENARIOS_WITH_FILES) {
       );
       t.same(depTree, expectedDepTree, 'Tree generated as expected');
     } catch (err) {
-      t.fail(err);
+      const error = err as Error;
+      t.fail(error);
     }
   });
 }
@@ -208,7 +209,8 @@ test(`Yarn Tree size exceeds the allowed limit of 500 dependencies (yarn1)`, asy
     );
     t.fail('Expected TreeSizeLimitError to be thrown');
   } catch (err) {
-    t.equals(err.constructor.name, 'TreeSizeLimitError');
+    const error = err as Error;
+    t.equals(error.constructor.name, 'TreeSizeLimitError');
   } finally {
     config.YARN_TREE_SIZE_LIMIT = 6.0e6;
   }
