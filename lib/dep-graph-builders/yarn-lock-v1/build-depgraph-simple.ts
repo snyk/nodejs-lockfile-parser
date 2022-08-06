1f9f2edc9e3c92662dc5b687bb0da01f8510cf4a
Xujia Zhou <xujia.zhou@snyk.io>
2022-08-06 18:12:01 +0200

    feat: use dfs to detect cycle and add nodes and edges to graph builder

--- lib/dep-graph-builders/yarn-lock-v1/build-depgraph-simple.ts
+++ lib/dep-graph-builders/yarn-lock-v1/build-depgraph-simple.ts
@@ -1,10 +1,21 @@
 import { DepGraphBuilder } from '@snyk/dep-graph';
 import { PackageJsonBase } from '../types';
 
-// Build dep graph from a parsed yarn.lock v1
-// for a non workspace project
+enum Color {
+  GRAY,
+  BLACK,
+}
+
+type Node = {
+  id: string;
+  name: string;
+  version: string;
+  dependencies: Record<string, { version: string; isDev: boolean }>;
+  isDev: boolean;
+};
+
 export const buildDepGraphYarnLockV1Simple = (
-  parsedYarnLockV1: Record<
+  extractedYarnLockV1Pkgs: Record<
     string,
     {
       version: string;
@@ -18,119 +29,121 @@ export const buildDepGraphYarnLockV1Simple = (
     { name: 'yarn' },
     { name: pkgJson.name, version: pkgJson.version },
   );
+
+  const colorMap: Record<string, Color> = {};
+
   const topLevelDeps = getTopLevelDeps(pkgJson, {
     includeDevDeps: options.includeDevDeps,
   });
-  // Start with the root node in our queue
-  const nodesForDepGraph = [
-    {
-      name: pkgJson.name,
-      version: pkgJson.version,
-      dependencies: topLevelDeps,
-      isDev: false,
-      isRoot: true,
-    },
-  ];
-  const ancestorMap: Record<string, Set<string>> = {};
+  const rootNode = {
+    id: 'root-node',
+    name: pkgJson.name,
+    version: pkgJson.version,
+    dependencies: topLevelDeps,
+    isDev: false,
+  };
 
-  const nodesPrunedAsCyclic: Set<string> = new Set();
+  dfsVisit(rootNode, colorMap, extractedYarnLockV1Pkgs, depGraphBuilder);
 
-  while (nodesForDepGraph.length > 0) {
-    const nodeData = nodesForDepGraph.shift();
-    const parentId = nodeData?.isRoot
-      ? 'root-node'
-      : `${nodeData!.name}@${nodeData!.version}`;
-
-    for (const [name, depInfo] of Object.entries(
-      nodeData?.dependencies || {},
-    )) {
-      const depData = parsedYarnLockV1[`${name}@${depInfo.version}`];
-
-      let childId = `${name}@${depData.version}`;
-      // Is it cyclic
-      const isCyclic =
-        ancestorMap.hasOwnProperty(parentId) &&
-        ancestorMap[parentId].has(childId);
-
-      ancestorMap[childId] = new Set([
-        ...(ancestorMap[parentId] || []),
-        ...(ancestorMap[childId] || []),
-        parentId,
-      ]);
-
-      // ...and check if we have already done so.
-      if (isCyclic) {
-        childId = `${childId}|1`;
-      }
-      if (!nodesPrunedAsCyclic.has(childId)) {
-        if (isCyclic) {
-          nodesPrunedAsCyclic.add(childId);
-        }
-        depGraphBuilder.addPkgNode(
-          { name, version: depData.version },
-          childId,
-          {
-            labels: {
-              scope: depInfo.isDev || nodeData?.isDev ? 'dev' : 'prod',
-              ...(isCyclic && { pruned: 'cyclic' }),
-            },
-          },
-        );
-      }
-
-      depGraphBuilder.connectDep(parentId, childId);
-
-      if (!isCyclic) {
-        const dependencies = Object.entries(depData.dependencies || {}).reduce(
-          (
-            acc: Record<string, { version: string; isDev: boolean }>,
-            [name, semver],
-          ) => {
-            acc[name] = { version: semver, isDev: depInfo.isDev };
-            return acc;
-          },
-          {},
-        );
-
-        nodesForDepGraph.push({
-          name,
-          dependencies,
-          isDev: nodeData?.isDev || false,
-          version: depData.version,
-          isRoot: false,
-        });
-      }
-    }
-  }
   return depGraphBuilder.build();
 };
 
+/**
+ * Use DFS to add all nodes and edges to the depGraphBuilder and prune cyclic nodes.
+ * The colorMap keep track of the state of node during traversal.
+ *  - If a node doesn't exist in the map, it means it hasn't been visited.
+ *  - If a node is GRAY, it means it has already been discovered but its subtree hasn't been fully traversed.
+ *  - If a node is BLACK, it means its subtree has already been fully traversed.
+ *  - When first exploring an edge, if it points to a GRAY node, a cycle is found and the GRAY node is pruned.
+ *     - A pruned node has id `${originalId}|1`
+ */
+const dfsVisit = (
+  node: Node,
+  colorMap: Record<string, Color>,
+  extractedYarnLockV1Pkgs: Record<
+    string,
+    {
+      version: string;
+      dependencies: Record<string, string>;
+    }
+  >,
+  depGraphBuilder: DepGraphBuilder,
+): void => {
+  colorMap[node.id] = Color.GRAY;
+
+  for (const [name, depInfo] of Object.entries(node.dependencies || {})) {
+    const depData = extractedYarnLockV1Pkgs[`${name}@${depInfo.version}`];
+
+    const childNode: Node = {
+      id: `${name}@${depData.version}`,
+      name: name,
+      version: depData.version,
+      dependencies: getGraphDependencies(depData.dependencies, depInfo.isDev),
+      isDev: depInfo.isDev,
+    };
+
+    if (!colorMap.hasOwnProperty(childNode.id)) {
+      addPkgNodeToGraph(childNode, false, depGraphBuilder);
+      dfsVisit(childNode, colorMap, extractedYarnLockV1Pkgs, depGraphBuilder);
+    } else if (colorMap[childNode.id] === Color.GRAY) {
+      // cycle detected
+      childNode.id = `${childNode.id}|1`;
+      addPkgNodeToGraph(childNode, true, depGraphBuilder);
+    }
+
+    depGraphBuilder.connectDep(node.id, childNode.id);
+  }
+
+  colorMap[node.id] = Color.BLACK;
+};
+
+/**
+ * Get top level dependencies from the given package json object which is parsed from a package.json file.
+ * This includes both prod dependencies and dev dependencies supposing includeDevDeps is supported.
+ */
 const getTopLevelDeps = (
   pkgJson: PackageJsonBase,
   options: { includeDevDeps: boolean },
 ): Record<string, { version: string; isDev: boolean }> => {
-  const prodDeps = Object.entries(pkgJson.dependencies || {}).reduce(
+  const prodDeps = getGraphDependencies(pkgJson.dependencies || {}, false);
+
+  const devDeps = options.includeDevDeps
+    ? getGraphDependencies(pkgJson.devDependencies || {}, true)
+    : {};
+
+  return { ...prodDeps, ...devDeps };
+};
+
+/**
+ * Converts dependencies parsed from the yarn.lock file to a dependencies object required by the graph.
+ * For example, { 'mime-db': '~1.12.0' } will be converted to { 'mime-db': { version: '~1.12.0', isDev: true/false } }.
+ */
+const getGraphDependencies = (dependencies: Record<string, string>, isDev) => {
+  return Object.entries(dependencies).reduce(
     (
       acc: Record<string, { version: string; isDev: boolean }>,
       [name, semver],
     ) => {
-      acc[name] = { version: semver, isDev: false };
+      acc[name] = { version: semver, isDev: isDev };
       return acc;
     },
     {},
   );
+};
 
-  const devDeps = options.includeDevDeps
-    ? Object.entries(pkgJson.devDependencies || {}).reduce(
-        (
-          acc: Record<string, { version: string; isDev: boolean }>,
-          [name, semver],
-        ) => {
-          acc[name] = { version: semver, isDev: true };
-          return acc;
-        },
-        {},
-      )
-    : {};
-  return { ...prodDeps, ...devDeps };
+const addPkgNodeToGraph = (
+  node: Node,
+  isCyclic: boolean,
+  depGraphBuilder: DepGraphBuilder,
+): void => {
+  depGraphBuilder.addPkgNode(
+    { name: node.name, version: node.version },
+    node.id,
+    {
+      labels: {
+        scope: node.isDev ? 'dev' : 'prod',
+        ...(isCyclic && { pruned: 'cyclic' }),
+      },
+    },
+  );
 };
