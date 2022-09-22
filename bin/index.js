831b241c3ff3884c637f3b427cdfc4ec5c2a6f18
Craig Andrews <candrews@integralblue.com>
2022-09-22 16:00:50 -0400

    fix: detect yarn.lock files in bin command

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const inspect = require('../dist/index')
 
-inspect.buildDepTreeFromFiles('./', 'package.json', 'package-lock.json')
+inspect.buildDepTreeFromFiles('./', 'package.json', 'package-lock.json', 'yarn.lock')
   .then((tree) => {
     console.log(JSON.stringify(tree));
   })
